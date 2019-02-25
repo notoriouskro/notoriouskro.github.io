d1932723fbca5822e7b29b3c5ccee8ed2779d424
Kyle Robert <notoriouskro@gmail.com>
2019-02-24 21:50:14 -0800

    resume added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,44 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDtKTOeqShjHe5vsOkevu7jslcB6lEIRdY",
-    authDomain: "bootcamp-fire.firebaseapp.com",
-    databaseURL: "https://bootcamp-fire.firebaseio.com",
-    projectId: "bootcamp-fire",
-    storageBucket: "bootcamp-fire.appspot.com",
-    messagingSenderId: "745415818494"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyA1JhruhVOwPr06d19V2AIs0E-gzXKgdqA",
+    authDomain: "portfoliocontact-6650c.firebaseapp.com",
+    databaseURL: "https://portfoliocontact-6650c.firebaseio.com",
+    projectId: "portfoliocontact-6650c",
+    storageBucket: "portfoliocontact-6650c.appspot.com",
+    messagingSenderId: "421280888974"
+  };
+  firebase.initializeApp(config);
 
 // Create a variable to reference the database
 var database = firebase.database();
 
 
 // Capture Button Click
-$("#add-employee").on("click", function (event) {
+$("#contact").on("click", function (event) {
     // Don't refresh the page!
     event.preventDefault();
 
 
     // Store user input.
 
-    var name = $('#name-input').val();
-    var role = $('#role-input').val();
-    var sDate = $('#date-input').val();
-    var mRate = $('#rate-input').val();
-    // var tBilled = ;
-
-
-
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message= $('#message').val();
+    
 
 
     // Initial Values
     database.ref().push({
         name: name,
-        role: role,
-        sDate: sDate,
-        mRate: mRate,
+        email: email,
+        message: message,
     });
 
 
 
-    $('#name-input').val('');
-    $('#role-input').val('');
-    $('#date-input').val('');
-    $('#rate-input').val('');
+    $('#name').val('');
+    $('#email').val('');
+    $('#message').val('');
 
 
 
@@ -52,32 +46,32 @@ $("#add-employee").on("click", function (event) {
 
 
 
-database.ref().on("child_added", function (snap) {
+// database.ref().on("child_added", function (snap) {
 
-    console.log(snap.val())
+//     console.log(snap.val())
 
-    var val = snap.val();
-    var name = val.name;
-    var role = val.role;
-    var mRate = val.mRate;
-    var sDate = (moment(val.sDate, 'YYYY/MM/DD').format('MM/DD/YYYY')) ;
-    var cDate = moment();
-    var monthsDiff = cDate.diff(sDate, 'month');
-    var tBilled = mRate * monthsDiff;
+//     var val = snap.val();
+//     var name = val.name;
+//     var role = val.role;
+//     var mRate = val.mRate;
+//     var sDate = (moment(val.sDate, 'YYYY/MM/DD').format('MM/DD/YYYY')) ;
+//     var cDate = moment();
+//     var monthsDiff = cDate.diff(sDate, 'month');
+//     var tBilled = mRate * monthsDiff;
     
     
-    var $row = $('<tr>');
-    $row.append('<td>' + name + '</td>');
-    $row.append('<td>' + role + '</td>');
-    $row.append('<td>' + sDate + '</td>');
-    $row.append('<td>' + monthsDiff + '</td>');
-    $row.append('<td>' + mRate + '</td>');
-    $row.append('<td>' + tBilled + '</td>');
-    $('tbody').append($row);
+//     var $row = $('<tr>');
+//     $row.append('<td>' + name + '</td>');
+//     $row.append('<td>' + role + '</td>');
+//     $row.append('<td>' + sDate + '</td>');
+//     $row.append('<td>' + monthsDiff + '</td>');
+//     $row.append('<td>' + mRate + '</td>');
+//     $row.append('<td>' + tBilled + '</td>');
+//     $('tbody').append($row);
 
 
 
-});
+// });
 
 
 
