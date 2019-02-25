de03047f561b76a674c36050bd30cfc1c60f0f3c
Kyle Robert <notoriouskro@gmail.com>
2019-02-24 23:41:43 -0800

    modaltime

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,51 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyA1JhruhVOwPr06d19V2AIs0E-gzXKgdqA",
-    authDomain: "portfoliocontact-6650c.firebaseapp.com",
-    databaseURL: "https://portfoliocontact-6650c.firebaseio.com",
-    projectId: "portfoliocontact-6650c",
-    storageBucket: "portfoliocontact-6650c.appspot.com",
-    messagingSenderId: "421280888974"
-  };
-  firebase.initializeApp(config);
+// Get the modal
+var modal1 = document.getElementById('myModal1');
 
-// Create a variable to reference the database
-var database = firebase.database();
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
 
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
 
-// Capture Button Click
-$("#contact").on("click", function (event) {
-    // Don't refresh the page!
-    event.preventDefault();
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
 
-    // Store user input.
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
 
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var message= $('#message').val();
-    
+// Get the modal
+var modal2 = document.getElementById('myModal2');
 
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
 
-    // Initial Values
-    database.ref().push({
-        name: name,
-        email: email,
-        message: message,
-    });
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 
-    $('#name').val('');
-    $('#email').val('');
-    $('#message').val('');
-
-
-
-});
-
-
-
-// database.ref().on("child_added", function (snap) {
-
-//     console.log(snap.val())
-
-//     var val = snap.val();
-//     var name = val.name;
-//     var role = val.role;
-//     var mRate = val.mRate;
-//     var sDate = (moment(val.sDate, 'YYYY/MM/DD').format('MM/DD/YYYY')) ;
-//     var cDate = moment();
-//     var monthsDiff = cDate.diff(sDate, 'month');
-//     var tBilled = mRate * monthsDiff;
-    
-    
-//     var $row = $('<tr>');
-//     $row.append('<td>' + name + '</td>');
-//     $row.append('<td>' + role + '</td>');
-//     $row.append('<td>' + sDate + '</td>');
-//     $row.append('<td>' + monthsDiff + '</td>');
-//     $row.append('<td>' + mRate + '</td>');
-//     $row.append('<td>' + tBilled + '</td>');
-//     $('tbody').append($row);
-
-
-
-// });
-
-
-
-//   If any errors are experienced, log them to console.
-//   function(errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   };
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
